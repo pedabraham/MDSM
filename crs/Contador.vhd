1e910107bf24932150f26e0787779a198cb79dec
CarlosOrozcoMunoz <carlos.orozco.ug@gmail.com>
2016-12-02 15:01:42 -0600

    Contador secuencial. Se agrego el caso en que la entrada es 000000000

--- crs/Contador.vhd
+++ crs/Contador.vhd
@@ -2,28 +2,36 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_arith.all;
 ---------------------------------------------------
-entity cont is 
+entity contador is
+generic(
+	n : INTEGER := 4
+); 
 port(
+	num : in std_logic_vector(8 downto 0);
 	clk : in std_logic;
 	CE  : in std_logic;
-	count : out std_logic_vector(3 downto 0)
+	count : out std_logic_vector(n-1 downto 0)
 );
-end cont;
+end contador;
 ---------------------------------------------------
-architecture simple of cont is
+architecture simple of contador is
 -------------------SIGNALS-------------------------
-signal N_S : std_logic_vector();
-signal P_S : std_logic_vector();
+signal N_S : std_logic_vector(n-1 downto 0);
+signal P_S : std_logic_vector(n-1 downto 0);
 ---------------------------------------------------
 begin 
-	comb : process()
+	comb : process(num, P_S, count)
 	begin 
-		if(P_S = "1001") then
-			N_S <= (others => '0');
-		else 
-			N_S <= P_S + 1;
+		if(num = "000000000")then 
+			
+		else
+			if(P_S = "1001") then
+				N_S <= (others => '0');
+			else 
+				N_S <= P_S + 1;
+			end if;
+			count <= P_S;
 		end if;
-	count <= P_S;
 	end process comb;
 
 	sequ : process(clk, CE, N_S)
