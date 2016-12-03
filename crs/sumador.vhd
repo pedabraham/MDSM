588371ef9c0e5d4c2fe993eb4604c6373984dcce
pedabraham <pedabraham@gmail.com>
2016-12-03 09:29:01 -0600

    Se añadio la parte secuencial al sumador

--- crs/sumador.vhd
+++ crs/sumador.vhd
@@ -5,19 +5,33 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity sumador 
 is generic(
-    n:integer:=8
+    n:integer:=30
 );
  port(
-    A: in STD_LOGIC_VECTOR(n-1 downto 0);
-    B: in STD_LOGIC_VECTOR(3 downto 0);
-    resultado:  out STD_LOGIC_VECTOR(29 downto 0)
+    A           : in    STD_LOGIC_VECTOR(n-1 downto 0);
+    clk         : in    STD_LOGIC;
+    CE          : in    STD_LOGIC;
+    resultado   : out   STD_LOGIC_VECTOR(n-1 downto 0)
 );
 end sumador;
 
 architecture Behavioral of sumador is
+
+signal N_S,B: STD_LOGIC_VECTOR(n-1 downto 0); --B es el estado presente
+
     begin
-        process (A,B)
+        comb: process (A,B)
         begin
-        resultado <= unsigned(A) +unsigned(B);
+            resultado <= unsigned(A) +unsigned(B);
+            N_S<=resultado
         end process;
+
+        sequ: process
+        begin
+            if (clk'event AND clk = '1') then
+			if (CE = '1') then
+				B <=	N_S; 
+			end if;
+		end if;
+
 end Behavioral;
