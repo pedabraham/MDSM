67012240410a29c4a766c776e2dfffddbb10f8ef
pedabraham <pedabraham@gmail.com>
2016-12-03 09:35:14 -0600

    Solución de unos errores en val1

--- crs/val1.vhd
+++ crs/val1.vhd
@@ -7,18 +7,18 @@ entity val1 is
     );
     port(
     NewWord         :   IN  STD_LOGIC_VECTOR(n-1 downto 0);	--cadena recien hecha
-    -- AlreadyInWord   :   IN  STD_LOGIC_VECTOR(n-1 downto 0);	--cadena de comprobacion
     CE              :   IN  STD_LOGIC;
     Clk             :   IN  STD_LOGIC;
-    -- FinalWord       :   OUT STD_LOGIC_VECTOR(n-1 downto 0);	--cadena de comprobacion nueva, regresa como entrada
     GoodWord        :   OUT STD_LOGIC_VECTOR(n-1 downto 0)	--palabra que saldra si pasa los filtros
     );
 end val1;
 
 architecture Behavioral of val1 is 
 begin
-    signal AlreadyInWord,FinalWord: STD_LOGIC_VECTOR(n-1 downto 0);
-    process (NewWord,AlreadyInWord)
+    signal AlreadyInWord    :   STD_LOGIC_VECTOR(n-1 downto 0); --cadena de comprobacion o estado presente
+    signal FinalWord        :   STD_LOGIC_VECTOR(n-1 downto 0); --cadena de comprobacion nueva o proximo estado
+
+    comb: process (NewWord,AlreadyInWord)
     begin
         for I in 0 to n-1 loop
             if(AlreadyInWord(I)='0') then
@@ -37,11 +37,11 @@ begin
     sequ: process(CE,Clk,FinalWord) 
     begin
 
-    if(Clk'event AND CE=1) then 
-        AlreadyInWord<=FinalWord;
-    end if;
-
-
+    if(Clk'event AND Clk='1') then 
+			if (CE = '1') then
+				AlreadyInWord<=FinalWord;
+			end if;
+	end if;
     end process; 
 
 
