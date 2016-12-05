14d9405aa7646bc5f6a89692cf183029ca61021d
CarlosOrozcoMunoz <carlos.orozco.ug@gmail.com>
2016-12-05 13:22:50 -0600

    !Incompleto¡ Base de tiempo para que el programa funcione cada que se detecta un sensor

---
+++ crs/BaseDeTiempo.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity BaseDeTiempo is
+	generic(
+		n : integer := 9
+	);
+	port(
+		CLK : IN STD_LOGIC;
+		NewWord : IN STD_LOGIC_VECTOR(n-1 downto 0);
+		CE  : OUT STD_LOGIC
+	);
+end BaseDeTiempo;
+
+architecture Behavioral of BaseDeTiempo is
+begin
+	
+
+
+end Behavioral;
