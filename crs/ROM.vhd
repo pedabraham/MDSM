db56679b8aecaf9fccf46ecfca4b2b7066ba741a
pedabraham <pedabraham@gmail.com>
2016-12-07 13:58:23 -0600

    se añadio la ROM y el verificador

---
+++ crs/ROM.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity ROM is 
+port(
+	Count : in STD_LOGIC_VECTOR(3 downto 0);
+	Valor : out STD_LOGIC_VECTOR(3 downto 0)
+);
+end ROM;
+
+Architecture Behavioral of ROM is
+begin
+	process(Count)
+	begin
+		case Count is
+			when "0001" =>	Valor <= "0001";
+			when "0010" =>	Valor <= "0010";
+			when "0011" =>	Valor <= "0011";
+			when "0100" =>	Valor <= "0101";
+			when "0101" =>	Valor <= "0111";
+			when "0110" =>	Valor <= "1000";
+			when "0111" =>	Valor <= "1001";
+			when others =>	Valor <= "0000";
+		end case;
+	end process;
+
+end Behavioral;
---
+++ crs/verificador.vhd
@@ -0,0 +1,30 @@
+--Comparadors
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity verificador 
+is generic(
+    n : integer := 4
+);
+port(
+    A: in STD_LOGIC_VECTOR(n-1 downto 0);
+    B: in STD_LOGIC_VECTOR(n-1 downto 0);
+    Count in STD_LOGIC_VECTOR(n-1 downto 0);
+    Bool: out STD_LOGIC
+);
+end verificador;
+
+architecture Behavioral of verificador is
+    begin
+        process (A,B)
+        begin
+            if( A = B) then
+                Bool    <= '1';
+            else
+                Bool    <='0';
+            end if;
+        end process;
+end Behavioral;
