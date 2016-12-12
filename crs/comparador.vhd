e2054f1a5cbdf1a86a5265e1346a1f395723f9c1
Carlos Alberto Orozco Muñoz <carlos.orozco.ug@gmail.com>
2016-12-12 03:05:30 -0600

    Nuevo Diseño.
    Se fusiono la entidad Comparador con Verificador1

--- crs/comparador.vhd
+++ crs/comparador.vhd
@@ -10,18 +10,31 @@ entity comparador is
     port(
         A: in STD_LOGIC_VECTOR(n-1 downto 0);
         B: in STD_LOGIC_VECTOR(n-1 downto 0);
+        CLK: in STD_LOGIC;
+        Clr : in STD_LOGIC;
+        Count : in STD_LOGIC_VECTOR(3 downto 0);
         Bool: out STD_LOGIC
     );
 end comparador;
 
 architecture Behavioral of comparador is
+signal N_S : std_logic_vector(9 downto 0);
+begin
+    process (A,B,CLK,Clr,Count)
     begin
-        process (A,B)
-        begin
+        if(CLK'event AND CLK = '1') then        
             if( A = B) then
-                Bool<= '1';
+                N_S(conv_integer(Count)) <= '1';
             else
-                Bool <='0';
+                N_S(conv_integer(Count)) <= '0';
             end if;
-        end process;
+        elsif(N_S(6 downto 0) = "1111111" )then
+            Bool <= '1';
+        elsif(Clr = '1')then
+            N_S <= (others => '0');
+            Bool <= '0';--
+        else
+            Bool <= '0';
+        end if;
+    end process;
 end Behavioral;
